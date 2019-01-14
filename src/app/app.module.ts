8101a16387728edf962f0fffa51c02a44e53b4d3
Satyam Gehani <Satyam.Gehani@nology.io>
2019-01-14 17:21:39 +0000

    weather fuckin works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from './home/home.component';
 import { WeatherComponent } from './weather/weather.component';
 import { QuizComponent } from './quiz/quiz.component';
@@ -25,7 +26,8 @@ import { FootballTeamComponent } from './football-team/football-team.component';
       { path: "weather", component: WeatherComponent},
       { path: "quiz", component: QuizComponent},
       { path: "football-team", component: FootballTeamComponent}
-    ])
+    ]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from "rxjs/operators";
+
+interface IWeather {
+  temp: number;
+  humidity: number;
+  minTemp: number;
+  maxTemp: number;
+}
+
+interface IWeatherAPI {
+  main: IWeather[];
+}
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +21,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  private forecast: Observable<IWeather>;
+  private error = false;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.forecast = this.http.get<IWeather>('http://api.openweathermap.org/data/2.5/weather?id=2643744&APPID=f4bbeb8ec1a8fb95591f2cf112aaa575');
+    
   }
 
 }
+
+// this.forecast = this.http.get<IWeather>('http://api.openweathermap.org/data/2.5/weather?id=2643744&APPID=f4bbeb8ec1a8fb95591f2cf112aaa575').pipe(
+//   catchError((err:HttpErrorResponse) => {
+//     this.error = true;
+//     return new Observable<IWeather>()
+//   })
+// );
+
